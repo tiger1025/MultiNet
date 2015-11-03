43518e7c002ef4bd4f5c11192c2920dcd0009d40
Tiger1025 <kirill-1025@mail.ru>
2015-11-03 15:21:50 +0700

    create socket server

--- MultiNet/MultiNet/BaseServer.cs
+++ MultiNet/MultiNet/BaseServer.cs
@@ -5,10 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.Net;
+using System.Net.Sockets;
 
 namespace MultiNet
 {
     class BaseServer
     {
+        Socket server = null;
+
+        public BaseServer()
+        {
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
     }
 }
