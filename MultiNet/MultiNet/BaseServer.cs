46c7d624899f1981845cd6647944c432c6eedfe8
Tiger1025 <kirill-1025@mail.ru>
2015-11-03 15:30:35 +0700

    socket server

--- MultiNet/MultiNet/BaseServer.cs
+++ MultiNet/MultiNet/BaseServer.cs
@@ -12,10 +12,26 @@ namespace MultiNet
     class BaseServer
     {
         Socket server = null;
+        int maxPlayers, connectionPort;
 
         public BaseServer()
         {
             server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void Run(int connectionPort, int maxPlayers)
+        {
+            this.connectionPort = connectionPort;
+            this.maxPlayers = maxPlayers;
+            server.Bind(new IPEndPoint(IPAddress.Any, connectionPort));
+            server.Listen(Math.Abs(maxPlayers)/ 10 + 1);
+            server.BeginAccept(new AsyncCallback(OnConnect), null);
+        }
+
+        private void OnConnect(IAsyncResult iar)
+        {
+            Socket client = server.EndAccept(iar);
+            server.BeginAccept(new AsyncCallback(OnConnect), null);
         }
     }
 }
